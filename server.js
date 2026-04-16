a81fc2a8eba90a331080ccf689137c88b35456f5
Evans Pereira <evanspereira26@gmail.com>
2026-04-16 18:37:27 +0530

    fix: removed hardcoded DNS override for vercel production

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const dns = require("dns");
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
+if (process.env.NODE_ENV !== 'production') {
+    const dns = require("dns");
+    dns.setServers(["8.8.8.8", "8.8.4.4"]);
+}
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
